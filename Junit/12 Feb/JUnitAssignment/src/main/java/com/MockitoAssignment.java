5424605984936dcfe1f012d30b2320dd5c10ae26
sdemadhav <madhav.jha@wissen.com>
2025-02-12 17:34:42 +0530

    12 Feb Updates

---
+++ Junit/12 Feb/JUnitAssignment/src/main/java/com/MockitoAssignment.java
@@ -0,0 +1,6 @@
+package com;
+
+public interface MockitoAssignment {
+	
+	public void xyz();
+}
---
+++ Junit/12 Feb/junitdemo/src/main/java/com/Calculator.java
@@ -0,0 +1,32 @@
+package com;
+
+public class Calculator {
+	
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	public int div(int a, int b) throws Exception
+	{
+		if(b!=0) {
+		return a/b;
+		}
+		
+		else {
+			throw new ArithmeticException();
+		}
+	}
+	
+	public int mod(int a, int b)
+	{
+		return a%b;
+	}
+}
