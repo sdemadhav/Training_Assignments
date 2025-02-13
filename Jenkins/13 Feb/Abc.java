f00893f9d1db581e2c56ec6191cd99f9386bbaf8
sdemadhav <madhav.jha@wissen.com>
2025-02-13 16:02:44 +0530

    Jenkins Updated Project

---
+++ Jenkins/13 Feb/Abc.java
@@ -0,0 +1,8 @@
+public class Abc{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello from File Abc.java");	
+		System.out.println("Compiling and Executing from Jenkins");	
+		System.out.println("***<<<----THANK YOU---->>>***");	
+	}
+}
